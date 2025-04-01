3cd2c1d12f610cd2a5ebb9c516a30577eb71ed51
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-01 09:33:58 +0400

    add IEventBus

---
+++ DoctorBooking/DoctorBooking.Shared/Core/IEventBus.cs
@@ -0,0 +1,10 @@
+﻿
+namespace DoctorBooking.Shared.Core
+{
+    public interface IEventBus
+    {
+        void Subscribe<T>(object subscriber, Type eventType, Action<T> handler);
+        void Unsubscribe<T>(object subscriber);
+        void Publish<T>(T @event);
+    }
+}
