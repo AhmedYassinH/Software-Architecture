9fe69c8f82fea2c477f27d39b6519ad3f76441f5
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-01 11:36:39 +0400

    add DoctorSlot Entity and initilizer

---
+++ DoctorBooking/DoctorBooking.DoctorAvailability/DAL/Initializer/Initializer.cs.cs
@@ -0,0 +1,58 @@
+﻿
+using DoctorBooking.DoctorAvailability.DAL.EFStructures;
+using DoctorBooking.DoctorAvailability.Models;
+
+
+namespace DoctorBooking.DoctorAvailability.DAL.Initializer
+{
+    public class Initializer
+    {
+        // 5 Dummy records 
+        public static List<DoctorSlotEntity> Slots = new()
+        {
+            new ()
+            {
+                Id = new Guid(),
+                DoctorId = Guid.Parse("a1b2c3d4-e5f6-4a5b-9c8d-1e2f3a4b5c6d"),
+                Time = new DateTime(2022, 10, 10, 10, 0, 0),
+                Cost = 100,
+                IsReserved = false
+            },
+            new ()
+            {
+                Id = new Guid(),
+                DoctorId = Guid.Parse("a1b2c3d4-e5f6-4a5b-9c8d-1e2f3a4b5c6d"),
+                Time = new DateTime(2022, 10, 10, 11, 0, 0),
+                Cost = 100,
+                IsReserved = false
+            },
+            new ()
+            {
+                Id = new Guid(),
+                DoctorId = Guid.Parse("a1b2c3d4-e5f6-4a5b-9c8d-1e2f3a4b5c6d"),
+                Time = new DateTime(2022, 10, 10, 12, 0, 0),
+                Cost = 100,
+                IsReserved = false
+            },
+            new ()
+            {
+                Id = new Guid(),
+                DoctorId = Guid.Parse("a1b2c3d4-e5f6-4a5b-9c8d-1e2f3a4b5c6d"),
+                Time = new DateTime(2022, 10, 10, 13, 0, 0),
+                Cost = 100,
+                IsReserved = false
+            },
+
+        };
+
+       
+
+        public static void InitializeData(AvailabilityDbContext context)
+        {
+            context.Database.EnsureCreated();
+            context.DoctorSlotEntities.AddRange(Slots);
+            context.SaveChanges();
+        }
+
+    }
+}
---
+++ DoctorBooking/DoctorBooking.DoctorAvailability/Models/DoctorSlotEntity.cs
@@ -0,0 +1,17 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DoctorBooking.DoctorAvailability.Models
+{
+    public class DoctorSlotEntity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public DateTime Time { get; set; }
+        public Guid DoctorId { get; set; }
+        public bool IsReserved { get; set; }
+        public decimal Cost { get; set; }
+    }
+}
