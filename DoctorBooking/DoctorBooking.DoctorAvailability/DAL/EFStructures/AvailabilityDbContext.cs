3fa5c889d92e4d7f92a82818564d52c3998cb674
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-01 11:36:54 +0400

    add AvailabilityDbContext for EF core

---
+++ DoctorBooking/DoctorBooking.DoctorAvailability/DAL/EFStructures/AvailabilityDbContext.cs
@@ -0,0 +1,15 @@
+﻿
+using DoctorBooking.DoctorAvailability.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DoctorBooking.DoctorAvailability.DAL.EFStructures
+{
+    public class AvailabilityDbContext : DbContext
+    {
+        public AvailabilityDbContext(DbContextOptions<AvailabilityDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DoctorSlotEntity> DoctorSlotEntities { get; set; }
+    }
+}
