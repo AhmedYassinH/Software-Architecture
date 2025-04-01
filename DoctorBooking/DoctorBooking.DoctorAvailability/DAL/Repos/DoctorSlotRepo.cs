f1ae4e746dc93efe39df6ff9c18fafb0ae2c7746
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-01 11:49:09 +0400

    add DoctorSlotRepo

---
+++ DoctorBooking/DoctorBooking.DoctorAvailability/DAL/Repos/DoctorSlotRepo.cs
@@ -0,0 +1,48 @@
+﻿
+
+using DoctorBooking.DoctorAvailability.DAL.EFStructures;
+using DoctorBooking.DoctorAvailability.Models;
+
+namespace DoctorBooking.DoctorAvailability.DAL.Repos
+{
+    public class DoctorSlotRepo
+    {
+        private readonly AvailabilityDbContext _context;
+        public DoctorSlotRepo(AvailabilityDbContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public List<DoctorSlotEntity> GetAllSlots()
+        {
+            return _context.DoctorSlots.ToList();
+        }
+
+        public void AddSlot(DoctorSlotEntity slot)
+        {
+            _context.DoctorSlots.Add(slot);
+            _context.SaveChanges();
+        }
+
+        public void ReserveSlot(DoctorSlotEntity slot)
+        {
+            var slotToReserve = _context.DoctorSlots.Find(slot.Id);
+            if (slotToReserve != null && !slotToReserve.IsReserved)
+            {
+                slotToReserve.IsReserved = true;
+                _context.SaveChanges();
+                return;
+
+            }
+            else
+            {
+                throw new Exception("Slot is already reserved");
+            }
+        }
+
+
+
+    }
+}
