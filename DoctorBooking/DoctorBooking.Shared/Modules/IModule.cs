ada0188d6961da16d62f109041c69c09867d5cad
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-01 09:11:23 +0400

    add DoctorBooking.Shared IModule

---
+++ DoctorBooking/DoctorBooking.Shared/Modules/IModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Routing;
+namespace DoctorBooking.Shared.Modules
+{
+    public interface IModule
+    {
+        void RegisterServices(IServiceCollection services);
+        void MapEndpoints(IEndpointRouteBuilder endpoints);
+        Task InitializeAsync();
+    }
+}
