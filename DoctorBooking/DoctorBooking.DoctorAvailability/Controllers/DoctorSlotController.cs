ff23e23707c33fe4272396a264b7762d5d877252
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-01 12:12:03 +0400

    add DoctorSlotontroller

---
+++ DoctorBooking/DoctorBooking.DoctorAvailability/Controllers/DoctorSlotController.cs
@@ -0,0 +1,28 @@
+﻿
+using DoctorBooking.DoctorAvailability.Models;
+using DoctorBooking.DoctorAvailability.Services;
+
+namespace DoctorBooking.DoctorAvailability.Controllers
+{
+    public class DoctorSlotController
+    {
+        private readonly DoctorSlotService _service;
+        public DoctorSlotController(DoctorSlotService service)
+        {
+            _service = service;
+        }
+  
+        public async Task<List<DoctorSlotEntity>> GetAllSlots()
+        {
+            return await _service.GetAllSlots();
+        }
+        public async Task AddSlotAsync(DoctorSlotEntity slot)
+        {
+            await _service.AddSlotAsync(slot);
+        }
+        public async Task ReserveSlotAsync(Guid slotId)
+        {
+            await _service.ReserveSlotAsync(slotId);
+        }
+    }
+}
