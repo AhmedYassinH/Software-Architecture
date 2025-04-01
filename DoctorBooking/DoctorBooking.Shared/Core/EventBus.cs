f4a7498e2fff4fc9fd32c6e105b2c4906526b35f
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-01 10:01:26 +0400

    add EventBus Implementation

---
+++ DoctorBooking/DoctorBooking.Shared/Core/EventBus.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace DoctorBooking.Shared.Core
+{
+    public class EventBus : IEventBus
+    {
+        private readonly List<EventListener> _eventListeners = new List<EventListener>();
+        public void Publish<T>(T @event)
+        {
+            lock (_eventListeners)
+            {
+                foreach (var listener in _eventListeners)
+                {
+                    if (listener.eventType == typeof(T))
+                    {
+                        var handler = (Action<T>)listener.handler;
+                        handler(@event);
+
+                    }
+                }
+            }
+        }
+
+        public void Subscribe<T>(object subscriber, Type eventType, Action<T> handler)
+        {
+            var eventListener = new EventListener(subscriber, eventType, handler);
+            lock (_eventListeners)
+            {
+                _eventListeners.Add(eventListener);
+            }
+        }
+
+        public void Unsubscribe<T>(object subscriber)
+        {
+            lock (_eventListeners)
+            {
+                _eventListeners.RemoveAll(x => x.subscriber == subscriber);
+            }
+        }
+
+
+        private record EventListener(object subscriber, Type eventType, Delegate handler);
+    }
+}
