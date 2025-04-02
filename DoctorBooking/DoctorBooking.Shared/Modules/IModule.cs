ec0015ccb04485cc6ad0c75f78a4361ec1d7277e
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-02 07:35:37 +0400

    Fix the IModule

--- DoctorBooking/DoctorBooking.Shared/Modules/IModule.cs
+++ DoctorBooking/DoctorBooking.Shared/Modules/IModule.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Builder;
 namespace DoctorBooking.Shared.Modules
 {
     public interface IModule
     {
         void RegisterServices(IServiceCollection services);
-        void MapEndpoints(IEndpointRouteBuilder endpoints);
-        Task InitializeAsync();
+        void MapEndpoints(IEndpointRouteBuilder builder);
+        void Initialize(WebApplication app);
     }
 }
