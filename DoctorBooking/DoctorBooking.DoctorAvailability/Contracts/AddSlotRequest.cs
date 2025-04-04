51f2f9dca942cd4a6e98c11709e6e01618460db1
AhmedYassin <dev.ahmedyassin@gmail.com>
2025-04-04 06:59:30 +0400

    add AddSlotRequest Contract dto

---
+++ DoctorBooking/DoctorBooking.DoctorAvailability/Contracts/AddSlotRequest.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace DoctorBooking.DoctorAvailability.Contracts
+{
+    public class AddSlotRequest
+    {
+        [Required]
+        public DateTime Time { get; set; }
+        
+        [Required]
+        public Guid DoctorId { get; set; }
+        
+        [Required]
+        public decimal Cost { get; set; }
+    }
+}
